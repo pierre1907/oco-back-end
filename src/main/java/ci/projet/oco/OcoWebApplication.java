e656b6b47391eeb029b8ef89ccbf4fc178c444d5
Saint-Pierre KASSI <eliakimksi@airdesantpierre.home>
2024-07-07 20:56:50 +0000

    init project and first commit

---
+++ src/main/java/ci/projet/oco/OcoWebApplication.java
@@ -0,0 +1,13 @@
+package ci.projet.oco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ci/projet/oco/OcoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.projet.oco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
